3938b3b9943412ffad6f3a35a79ca02f56932b48
MingyeomKim <mingyum119@naver.com>
2022-05-20 16:02:58 +0900

    fixed untracked files

--- src/main/java/com/example/book/springboot/web/dto/PostsListResponseDto.java
+++ src/main/java/com/example/book/springboot/web/dto/PostsListResponseDto.java
@@ -1,2 +1,22 @@
-package com.example.book.springboot.web.dto;public class PostsListReponseDto {
+package com.example.book.springboot.web.dto;
+
+import com.example.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    // convert Posts to PostsListResponseDto
+    public PostsListResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
 }
